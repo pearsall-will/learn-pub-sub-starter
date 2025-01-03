626ee95df6803faf92b492f65b34766b6fa2e1e0
Will Pearsall <pearsallwilliam@gmail.com>
2025-01-03 15:33:22 -0500

    regular checkin.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+
 	fmt.Println("Starting Peril server...")
+	con, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println("Connection failed!!!")
+		fmt.Println(err)
+	} else {
+		defer con.Close()
+		fmt.Println("Connection successful...")
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, os.Interrupt)
+		<-sigChan
+		fmt.Println("Closing Terminal...")
+	}
 }
